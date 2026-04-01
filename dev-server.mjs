95d473ecbc98b58ba10981ee09e99000467e5ebd
uhaszYsz <tengolibroda@gmail.com>
2026-04-01 08:00:39 +0200

    Update dev-server.mjs

--- dev-server.mjs
+++ dev-server.mjs
@@ -90,6 +90,7 @@ const CORS_API = {
 const MIME = {
     '.html': 'text/html; charset=utf-8',
     '.js': 'text/javascript; charset=utf-8',
+    '.mjs': 'text/javascript; charset=utf-8',
     '.json': 'application/json; charset=utf-8',
     '.css': 'text/css; charset=utf-8',
     '.ico': 'image/x-icon',
